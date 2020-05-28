8b20aec7d1e4cf9d78dc5de49ccaaa709eb530cc
solwininfotech <info@solwininfotech.com>
2020-05-28 19:19:44 +0530

    Initial commit

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin Hide Product Price Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_HidePrice
+ * @copyright  Copyright © 2006-2016 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_HidePrice',
+    __DIR__
+);
